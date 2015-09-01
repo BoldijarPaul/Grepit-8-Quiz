3e5194f594d90dbbaca5064627bdaf882ee56e00
Boldijar Paul <paul.bv96@yahoo.com>
2015-09-01 16:58:08 +0300

    made methods static

--- src/main/java/com/boldijarpaul/grepit8/service/AnswersManager.java
+++ src/main/java/com/boldijarpaul/grepit8/service/AnswersManager.java
@@ -12,7 +12,7 @@ public class AnswersManager {
 	private static List<QuestionResponse> answers = new ArrayList<QuestionResponse>();
 
 	/* this method will add a new answer to our list */
-	public void addAnswer(int questionId, int answerId, String answer,
+	public static void addAnswer(int questionId, int answerId, String answer,
 			String fullName) {
 		QuestionResponse questionResponse = new QuestionResponse();
 		questionResponse.setAnswer(answer);
@@ -29,7 +29,7 @@ public class AnswersManager {
 	 * @param questionId the id of the question we want to get the answers
 	 * @return a list of answers
 	 */
-	public List<QuestionResponse> getQuestionAnswers(int questionId) {
+	public static List<QuestionResponse> getQuestionAnswers(int questionId) {
 		List<QuestionResponse> questionAnswers = new ArrayList<QuestionResponse>();
 		for (QuestionResponse questionResponse : answers) {
 			if (questionResponse.getQuestionId() == questionId) {
