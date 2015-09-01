610b1f86bf5c71ef545e5957bbe3d638a8dfb9c3
Boldijar Paul <paul.bv96@yahoo.com>
2015-09-01 18:02:48 +0300

    added method to retrieve answers

--- frontend/scripts/apihelper.js
+++ frontend/scripts/apihelper.js
@@ -5,11 +5,35 @@ function addAnswer(questionId,answer,fullName){
         type: "GET",
         success : handleData,
         error: handleError
-    })
+    });
 }
 function handleData(data, textStatus, xhr) {
 	alert("success");
 }
 function handleError(xhr, textStatus, errorThrown) {
 	alert(xhr.responseText);
+}
+
+function getQuestionAnswers(questionId){
+    $.ajax({
+        url : home+"/answer/"+questionId+"/all",
+        type: "GET",
+        success : gotQuestions,
+        error: handleError
+    });
+}
+
+function gotQuestions(data,textStatus,xhr){
+   htmlText="<table class=\"table\"><thead><tr><th>Nume complet</th><th>Raspuns</th><th>Data</th></tr></thead>";
+   htmlText+="<tbody>";
+   data.forEach(function(obj) {
+    htmlText+="<tr>";
+    htmlText+="<td>"+obj.fullName+"</td>";
+    htmlText+="<td>"+obj.answer+"</td>";
+     htmlText+="<td>"+obj.createdDate+"</td>";
+    htmlText+="</tr>";
+   });
+   htmlText+="</tbody></table>";
+    $("#answers").html(htmlText);
+
 }
