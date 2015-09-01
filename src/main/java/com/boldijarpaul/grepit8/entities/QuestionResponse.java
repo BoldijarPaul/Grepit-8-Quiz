61ec2b4931a5408e747b86d0350ad0887a3e83ef
Boldijar Paul <paul.bv96@yahoo.com>
2015-09-01 16:41:52 +0300

    added the QuestionResponse.java entity

---
+++ src/main/java/com/boldijarpaul/grepit8/entities/QuestionResponse.java
@@ -0,0 +1,13 @@
+package com.boldijarpaul.grepit8.entities;
+
+import java.sql.Timestamp;
+
+public class QuestionResponse {
+
+	/* raspunsul unei intrebari*/
+	private int questionId;
+	private int answerId;
+	private String answer;
+	private String fullName;
+	private Timestamp createdDate;
+}
