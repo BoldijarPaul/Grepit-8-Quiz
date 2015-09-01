5fa26b11954cb181e73a0f115bec0616a2488c1d
Boldijar Paul <paul.bv96@yahoo.com>
2015-09-01 16:42:10 +0300

    removed unused imports

--- src/main/java/com/boldijarpaul/grepit8/entities/QuestionResponse.java
+++ src/main/java/com/boldijarpaul/grepit8/entities/QuestionResponse.java
@@ -4,10 +4,41 @@ import java.sql.Timestamp;
 
 public class QuestionResponse {
 
-	/* raspunsul unei intrebari*/
+	/* raspunsul unei intrebari */
 	private int questionId;
 	private int answerId;
 	private String answer;
 	private String fullName;
 	private Timestamp createdDate;
+	public int getQuestionId() {
+		return questionId;
+	}
+	public void setQuestionId(int questionId) {
+		this.questionId = questionId;
+	}
+	public int getAnswerId() {
+		return answerId;
+	}
+	public void setAnswerId(int answerId) {
+		this.answerId = answerId;
+	}
+	public String getAnswer() {
+		return answer;
+	}
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public Timestamp getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(Timestamp createdDate) {
+		this.createdDate = createdDate;
+	}
+
 }
